94179cfdf23a56ac9cceabe742c2589220480a24
andrii-bodnar <29282228+andrii-bodnar@users.noreply.github.com>
2021-06-16 11:30:12 +0000

    Deploying to gh-pages from @ andrii-bodnar/crowdin-in-context-demo@a3764af7c3125b29a40bb053addae95eaf5b7d9f 🚀

--- src/components/LocaleSwitcher.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="locale-switcher">
-    <select v-model="$i18n.locale">
-      <option value="en">English</option>
-      <option value="uk">Ukrainian</option>
-      <option value="ach">In-Context</option>
-    </select>
-  </div>
-</template>
